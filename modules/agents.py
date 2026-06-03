3dbbdd747eddb3bec94f3d49d5190045ce44b3c1
marant482 <ma.dudzinski@student.uw.edu.pl>
2026-06-03 15:13:30 +0200

    Create agents.py

---
+++ modules/agents.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class MLPAgent(nn.Module):
+    def __init__(self, obs_dim, n_actions, hidden_dim=128):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, n_actions)
+        )
+
+    def forward(self, obs):
+        # obs shape: (batch_size, obs_dim)
+        return self.net(obs)
