fd4e8214f1de8fb67a49f29c869b6db87ea3b738
marant482 <ma.dudzinski@student.uw.edu.pl>
2026-06-04 02:56:20 +0200

    Update explorers.py

--- modules/explorers.py
+++ modules/explorers.py
@@ -23,6 +23,7 @@ class GaussianCopulaExplorer(BaseExplorer):
         # Zależnie od Waszych badań, to rho będzie wektorem/macierzą wyliczaną dynamicznie
         self.Sigma = np.full((self.n_agents, self.n_agents), rho)
         np.fill_diagonal(self.Sigma, 1.0)
+        self.Sigma += np.eye(self.n_agents) * 1e-6 # Zabezpieczenie PSD
 
     def should_explore(self, epsilon):
         u = np.random.multivariate_normal(np.zeros(self.n_agents), self.Sigma)
